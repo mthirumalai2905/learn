f9459df222593826be0affdaa959076ff118052b
mthirumalai2905 <mthirumalai2905@gmail.com>
2024-01-09 16:15:47 +0530

    basic api fetching

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import axios from 'axios'
+import { useState } from 'react';
+import React from 'react'
+
+const App = () => {
+    const [quote, setQuote] = useState('');
+    const getQuote = () =>{
+        axios.get('https://api.quotable.io/random')
+        .then(res => {
+           console.log(res.data.content);
+           setQuote(res.data.content)
+        }).catch(err => {
+           console.log(err);
+        })
+    }
+  return (
+    <div className='App'>
+        <button onClick={getQuote}>get a Quote
+        </button> 
+        <p id='tag1'>{quote}</p> 
+    </div>
+  );
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
+// Import React and ReactDOM
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// Import your main App component (replace 'App' with your actual component name)
+import App from './App';
+
+// Render the App component into the root element in your HTML file
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
