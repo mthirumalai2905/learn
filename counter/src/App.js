4885aabf5c89d801305c29852372e7f1337aa310
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-02-02 17:41:26 +0530

    counter adder

---
+++ counter/src/App.js
@@ -0,0 +1,23 @@
+import React,{useState} from 'react'
+import './App.css'
+const App = () => {
+  const[Counter, setCounter] = useState(0);
+
+  const handleCount = () =>{
+    setCounter(Counter + 2);
+
+    if(Counter === 50){
+      alert("You have reached 50");
+    }
+  }
+  return (
+    <div className='container'>
+    <p>{Counter}</p>
+      <button className='button' onClick={handleCount}>Count</button>
+      <div className='loader'></div>
+    
+    </div>
+  )
+}
+
+export default App
